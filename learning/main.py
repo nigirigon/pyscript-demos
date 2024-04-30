29d092b67f4887cf8635b03ec5245e04c0891e21
nigirigon <163670123+nigirigon@users.noreply.github.com>
2024-04-30 13:13:59 +0200

    maj

--- learning/main.py
+++ learning/main.py
@@ -1,3 +1,12 @@
 from pyscript import display
 m="Ready to play !"
 display(m, target="main_pyscript")
+
+
+from shiny.express import input, render, ui
+
+ui.input_slider("val", "Slider label", min=0, max=100, value=50)
+
+@render.text
+def slider_val():
+    return f"Slider value: {input.val()}"
