69035359411ec6b218faad611f98c94dbb8a7ad8
nigirigon <163670123+nigirigon@users.noreply.github.com>
2024-05-01 00:55:55 +0200

    maj

--- learning/main.py
+++ learning/main.py
@@ -2,28 +2,39 @@ from pyscript import display
 m="Ready to play !"
 display(m, target="main_pyscript")
 
+import modin.pandas as pd
+import pandas
 
-from shiny import App, Inputs, Outputs, Session, render, ui
+#############################################
+### For the purpose of timing comparisons ###
+#############################################
+import time
+import ray
+# Look at the Ray documentation with respect to the Ray configuration suited to you most.
+ray.init()
+#############################################
 
-app_ui = ui.page_fluid(
-    ui.input_select(
-        "state",
-        "Choose a state:",
-        {
-            "East Coast": {"NY": "New York", "NJ": "New Jersey", "CT": "Connecticut"},
-            "West Coast": {"WA": "Washington", "OR": "Oregon", "CA": "California"},
-            "Midwest": {"MN": "Minnesota", "WI": "Wisconsin", "IA": "Iowa"},
-        },
-    ),
-    ui.output_text("value"),
-)
+# This may take a few minutes to download
+import urllib.request
+dataset_url = "https://modin-datasets.intel.com/testing/yellow_tripdata_2015-01.csv"
+urllib.request.urlretrieve(dataset_url, "taxi.csv")
+
+start = time.time()
+
+pandas_df = pandas.read_csv(dataset_url, parse_dates=["tpep_pickup_datetime", "tpep_dropoff_datetime"], quoting=3)
+
+end = time.time()
+pandas_duration = end - start
+print("Time to read with pandas: {} seconds".format(round(pandas_duration, 3)))
 
 
-def server(input: Inputs, output: Outputs, session: Session):
-    @render.text
-    def value():
-        return "You choose: " + str(input.state())
+#
+start = time.time()
 
+modin_df = pd.read_csv(dataset_url, parse_dates=["tpep_pickup_datetime", "tpep_dropoff_datetime"], quoting=3)
 
-app = App(app_ui, server)
-display(app.run())
+end = time.time()
+modin_duration = end - start
+print("Time to read with Modin: {} seconds".format(round(modin_duration, 3)))
+
+print("Modin is {}x faster than pandas at `read_csv`!".format(round(pandas_duration / modin_duration, 2)))
