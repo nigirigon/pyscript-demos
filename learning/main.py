c26b1b2333e146cc79d1246f0bc373608c822aec
nigirigon <163670123+nigirigon@users.noreply.github.com>
2024-06-05 13:24:06 +0200

    test

--- learning/main.py
+++ learning/main.py
@@ -1,8 +1,11 @@
 from pyscript import display
 m="Ready to play !"
-display(m, target="main_pyscript")
+#display(m, target="main_pyscript")
 
 #import modin.pandas as pd
-import pandas as pd
+#import pandas as pd
+#display("test")
+
+import dask.dataframe as dd
 
 display("test")
